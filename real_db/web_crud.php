b77092113b65db47fbe049f73562ef89c8402ace
samsidmm2 <47064793+samsidmm2@users.noreply.github.com>
2020-03-16 15:45:00 -0500

    All of the Data in the Same Table

--- real_db/web_crud.php
+++ real_db/web_crud.php
@@ -37,12 +37,14 @@
 			<br>
 			<br>
 			<!--Beginning of the About Page components-->
-			<h2>For About Page</h2>
+			<h2>For The Web Page</h2>
 			<table class="table">
 				<thead>
 					<tr>
-						<th>Heading</th>
-						<th>Paragraph</th>
+						<th>Heading (About Page)</th>
+						<th>Paragraph (About Page)</th>
+						<th>Heading (Membership Page)</th>
+						<th>Paragraph (Membership Page)</th>	
 						<th colspan="2">Action</th>
 					</tr>
 				</thead>
@@ -52,6 +54,8 @@
 					<tr>
 						<td><?php echo $row['heading']; ?></td>
 						<td><?php echo $row['paragraph']; ?></td>
+						<td><?php echo $row['mem_heading']; ?></td>
+						<td><?php echo $row['mem_paragraph']; ?></td>
 						<td>
 							<a href="web_crud.php?edit=<?php echo $row['id']; ?>" class="btn btn-info">Edit</a>
 							<a href="web_process.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
@@ -74,12 +78,20 @@
 		<input type="hidden" name="id" value="<?php echo $id; ?>">
 			<div class="form-group">
 				<!--For About Page-->
-				<label>Heading</label>
+				<label>Heading (About Page)</label>
 				<input type="text" name="heading" class="form-control" value="<?php echo $heading; ?>" placeholder="Enter the heading for the About Page">
 			</div>
 			<div class="form-group">
-				<label>Paragraph</label>
+				<label>Paragraph (About Page)</label>
 				<textarea name="paragraph" class="form-control form-control-lg" value="<?php echo $paragraph; ?>" placeholder="Enter the paragraph for the About Page"></textarea>
+			</div>
+			<div class="form-group">
+						<label>Heading (Membership Page)</label>
+						<input type="text" name="mem_heading" class="form-control" value="<?php echo $mem_heading; ?>" placeholder="Enter the heading for the Membership Page">
+			</div>
+			<div class="form-group">
+						<label>Paragraph Heading (Membership Page)</label>
+						<textarea name="mem_paragraph" class="form-control form-control-lg" value="<?php echo $mem_paragraph; ?>" placeholder="Enter the paragraph for the Membership Page" ></textarea>
 			</div>
 			<div class="form-group">
 			<?php 
@@ -96,7 +108,7 @@
 			<br>
 			<br>
 			<!--------------------------------------------------------------------------------------------------------------------------------->
-			<!--Beginning of the Membnership Page components-->
+			<!--Beginning of the Membnership Page components>
 			<div class="row" id="memberships_components">
 			<br>
 			<br>
@@ -108,21 +120,21 @@
 						<th>Paragraph</th>
 						<th colspan="2">Action</th>
 					</tr>
-				</thead>
+				</thead-->
 
-				<?php
+				<!--?php
 				while ($row = $result->fetch_assoc()): ?>
 					<tr>
-						<td><?php echo $row['mem_heading']; ?></td>
-						<td><?php echo $row['mem_paragraph']; ?></td>
+						<td><//?php echo $row['mem_heading']; ?></td>
+						<td><//?php echo $row['mem_paragraph']; ?></td>
 						<td>
-							<a href="web_crud.php?edit=<?php echo $row['id']; ?>" class="btn btn-info">Edit</a>
-							<a href="web_process.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+							<a href="web_crud.php?edit=<//?php echo $row['id']; ?>" class="btn btn-info">Edit</a>
+							<a href="web_process.php?delete=<//?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
 						</td>	
-					</tr>
-			<?php endwhile; ?>		
+					</tr-->
+			<!--?php endwhile; ?>		
 			</table>
-			</div>
+			</div-->
 	<!--?php 
 		function pre_r( $array ) {
 		echo '<pre>';
@@ -131,28 +143,28 @@
 		}
 		?--->
 
-			<div class="justify-content-center">
+			<!--div class="justify-content-center">
 				<form method="POST" action="web_process.php">
-					<input type="hidden" name="id" value="<?php echo $id; ?>" >
+					<input type="hidden" name="id" value="</*?php echo $id; ?*/>" >
 					<div class="form-group">
-						<label>Heading</label>
-						<input type="text" name="mem_heading" class="form-control" value="<?php echo $mem_heading; ?>" placeholder="Enter the heading for the Membership Page">
+						<label>Heading (Membership Page)</label>
+						<input type="text" name="mem_heading" class="form-control" value="</*?php echo $mem_heading; ?*/>" placeholder="Enter the heading for the Membership Page">
 					</div>
 					<div class="form-group">
-						<label>Paragraph</label>
-						<textarea name="mem_paragraph" class="form-control form-control-lg" value="<?php echo $mem_paragraph; ?>" placeholder="Enter the paragraph for the Membership Page" ></textarea>
+						<label>Paragraph Heading (Membership Page)</label>
+						<textarea name="mem_paragraph" class="form-control form-control-lg" value="</*?php echo $mem_paragraph; ?*/>" placeholder="Enter the paragraph for the Membership Page" ></textarea>
 					</div>
 					<div class="form-group">
-					<?php 
+					</*?php 
 					if ($update == true):
-						?>
+						?*/>
 						<button type="submit" class="btn btn-info" name="update">Update</button>
-					<?php else: ?>
+					</*?php else: ?*/>
 					<button type="submit" class="btn btn-primary" name="save">Save</button>
-					<?php endif; ?>
+					</*?php endif; ?*/>
 					</div>
 				</form>
-			</div>		
+			</div--->		
 				
 			</div>
 	</div>
