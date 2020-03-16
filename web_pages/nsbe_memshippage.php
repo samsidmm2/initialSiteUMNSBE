c33b72b6d28c382efea16673439e36904f52e674
samsidmm2 <47064793+samsidmm2@users.noreply.github.com>
2020-03-16 00:28:20 -0500

    Add files via upload
    The membership page addition.

---
+++ web_pages/nsbe_memshippage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Membership</title>
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<title></title>
+</head>
+<body>
+	<div class="container">
+	<?php
+		$mysqli = new mysqli('localhost','root','','crud') or die(mysqli_error($mysqli));
+		$result = $mysqli->query("SELECT * FROM page_data") or die($mysqli->error);
+	?>
+	<div id="tabs">
+		<nav class="nav">
+			<li class="nav-item">
+				<a class="nav-link disabled" href="#">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link disabled" href="#">About</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link disabled" href="#">Membership</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+			</li>
+		</nav>
+	</div>
+<table class="table table table-borderless">
+	<!--thead>
+			<tr>
+				<th class="row justify-content-center">Heading</th>
+				<th class="row justify-content-center">Paragraph</th>
+			</tr>
+	</thead-->
+
+	<?php
+	while ($row = $result->fetch_assoc()): ?>
+		<tr>
+			<td class="row justify-content-center"><?php echo $row['mem_heading']; ?></td>
+			<td class="row"><?php echo $row['mem_paragraph']; ?></td>
+			</tr>
+		<?php endwhile; ?>	
+
+</table>
+</div>
+</body>
+</html>
